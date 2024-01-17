763e3f98a012b912b1c56037f7f2601c31f584de
Álvaro González <alvgonz.ojeda@gmail.com>
2024-01-17 17:24:30 +0100

    [dev] finished interaction system

--- scripts/player/crosshair.gd
+++ scripts/player/crosshair.gd
@@ -5,14 +5,14 @@ extends Control
 @export var DOT_RADIUS: float = 2.0
 @export var DOT_COLOR: Color = Color.WHITE
 
-var HEAD_RAYCAST_COLLIDING: bool = false
+var INTERACTOR_COLLIDING: bool = false
 
 func _ready() -> void:
 	interactor.connect("colliding", toogle_raycast)
 	queue_redraw()
 
 func _process(delta: float) -> void:
-	if HEAD_RAYCAST_COLLIDING:
+	if INTERACTOR_COLLIDING:
 		hide()
 	else:
 		show()
@@ -21,4 +21,4 @@ func _draw() -> void:
 	draw_circle(get_viewport_rect().size / 2, DOT_RADIUS, DOT_COLOR)
 
 func toogle_raycast(value: bool) -> void:
-	HEAD_RAYCAST_COLLIDING = value
+	INTERACTOR_COLLIDING = value
